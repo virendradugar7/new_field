12657975a48e65e9bc76fcef098b0dc52604de4b
Viru <virendradugar007@gmail.com>
2021-05-03 13:01:27 +0530

    first commit

---
+++ src/extensions/fielCustom/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IFielCustomFieldCustomizerStrings {
+  Title: string;
+}
+
+declare module 'FielCustomFieldCustomizerStrings' {
+  const strings: IFielCustomFieldCustomizerStrings;
+  export = strings;
+}
